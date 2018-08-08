85f27fd7e7a5ababeeef25e39837f3363d3854ca
vtoebe <vitoriatoebe@gmail.com>
2018-08-08 09:09:53 -0300

    Header Show/Hide (not done)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 function changeZindex() {
   document.getElementById("featuredVideo").style.zIndex = "2";
+  document.getElementById("logo").style.top = "100%";
 
   var iframe = document.querySelector('iframe');
   var player = new Vimeo.Player(iframe);
   player.play();
 }
 
-function showLogo() {
-  document.getElementById("logo").style.visibility = "visible";
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
 }
