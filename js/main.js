dab4ecd3a706e44b174d51c9f163c5fad9bc108a
vtoebe <vitoriatoebe@gmail.com>
2018-08-08 07:45:18 -0300

    Custom Play Button Interaction with Vimeo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 function changeZindex() {
   document.getElementById("featuredVideo").style.zIndex = "2";
+
+  var iframe = document.querySelector('iframe');
+  var player = new Vimeo.Player(iframe);
+  player.play();
 }
 
 function showLogo() {
